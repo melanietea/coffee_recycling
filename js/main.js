3b9323214bef17511d4df172b23fe800da775dbe
melanietea <melaniewctan@gmail.com>
2017-03-13 13:19:50 -0700

    QUERY THIS
    Look ma, I did it!

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+$("#jjbean").click( function(){
+  $("#jjbean_page").show( "slow" );
+  $("#coffee_menu").hide( "slow" );
+})
 
+$("#starbs").click( function(){
+  $("#starbucks_page").show( "slow" );
+  $("#coffee_menu").hide( "slow" );
+})
+
+$("#start").click( function(){
+  $("#coffee_menu").show( "slow" );
+  $("#welcome_page").hide( "slow" );
+})
+
+$("#thechoice").click( function(){
+  $("#coffee_menu").show( "slow" );
+  $("#jjbean_page").hide( "slow" );
+})
+
+$("#thechoice_starbs").click( function(){
+  $("#coffee_menu").show( "slow" );
+  $("#starbucks_page").hide( "slow" );
+})
